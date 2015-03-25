48eab0cdac1994020065f568dc50cc3458659748
jmartin <jmartin@zikzakmedia.com>
2015-03-25 16:01:23 +0100

    Changed purchase_request name in files by supply_request

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.pool import Pool
-from .purchase_request import *
+from .supply_request import *
 
 def register():
     Pool.register(
